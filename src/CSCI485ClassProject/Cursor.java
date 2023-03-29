a3dbad61380fd3f5ebc1936d9b2965f5776946ec
Andre Hsueh <andrehsu@usc.edu>
2023-03-28 23:29:22 -0700

    Update Cursor.java

--- src/CSCI485ClassProject/Cursor.java
+++ src/CSCI485ClassProject/Cursor.java
@@ -5,21 +5,5 @@ public interface Cursor {
     READ,
     READ_WRITE
   }
-  
-  public Record getFirst();
-  
-  public Record getLast();
-  
-  public Record getNext();
-  
-  public Record getPrevious();
-  
-  public StatusCode updateRecord(String[] attrNames, Object[] attrValues);
-  
-  public StatusCode deleteRecord();
-  
-  public StatusCode commitCursor();
-  
-  public StatusCode abortCursor();
 }
 
