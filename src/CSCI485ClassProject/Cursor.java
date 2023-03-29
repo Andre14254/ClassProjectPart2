a181a4e084a51f5f9e2b79e88c38c957ec2189ea
Andre Hsueh <andrehsu@usc.edu>
2023-03-28 19:01:42 -0700

    Update Cursor.java

--- src/CSCI485ClassProject/Cursor.java
+++ src/CSCI485ClassProject/Cursor.java
@@ -5,4 +5,35 @@ public class Cursor {
     READ,
     READ_WRITE
   }
+  
+  private final String tableName;
+  private final RecordStore recordStore;
+  private RecordCursor<FDBRecord> recordCursor;
+  private boolean isDirty = false;
+
+  public Cursor(String tableName, RecordStore recordStore, RecordCursor<FDBRecord> recordCursor) {
+    this.tableName = tableName;
+    this.recordStore = recordStore;
+    this.recordCursor = recordCursor;
+  }
+
+  public String getTableName() {
+    return tableName;
+  }
+
+  public RecordStore getRecordStore() {
+    return recordStore;
+  }
+
+  public RecordCursor<FDBRecord> getRecordCursor() {
+    return recordCursor;
+  }
+
+  public boolean isDirty() {
+    return isDirty;
+  }
+
+  public void setDirty(boolean dirty) {
+    isDirty = dirty;
+  }
 }
